c024f62e8c36825d30bee5885fa295c91b50e930
BrenoHReis <breno.reish@hotmail.com>
2022-03-22 21:09:53 -0300

    atualizado

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -16,47 +16,58 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Quadras Sports"),
       ),
-      body: Container(),
-      drawer: Drawer(
-        backgroundColor: Colors.black,
-        elevation: 5,
-        child: Container(
-          child: Column(
-            children: [
-              Container(
-                width:double.infinity,
-                height: 140,
-                child: Card(
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Column(
-                        children: [
-              CircleAvatar(
-                      
-                      radius: 55.0,
-                      backgroundImage: NetworkImage("https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80")
-              ),
-              Text("Seu Nome"),
-                        ],
-                      ),
-                      Positioned(
-                        right: 2,
-                        top: 2,
-                        child: IconButton(onPressed: (){
-
-                            Navigator.of(context).push( MaterialPageRoute(builder: (context)=>UsuarioPage(usuario:usuario)) );
-
-
-                        }, icon: Icon(Icons.edit)))
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("Bem Vindo!!"),
         ),
       ),
-    );
+      drawer: Drawer(
+        backgroundColor: Color.fromARGB(255, 0, 3, 0),
+        elevation: 5,
+        child: Container(
+          child: IconButton(
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => UsuarioPage(usuario: usuario)));
+            },
+            icon: Icon(Icons.exit_to_app),)
+            children: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 140,
+                  child: Card(
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            CircleAvatar(
+                                radius: 55.0,
+                                backgroundImage: NetworkImage(
+                                    "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80")),
+                            Text("Seu Nome"),
+                          ],
+                        ),
+                        Positioned(
+                            right: 2,
+                            top: 2,
+                            child: IconButton(
+                                onPressed: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) =>
+                                          UsuarioPage(usuario: usuario)));
+                                },
+                                icon: Icon(Icons.edit)))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
